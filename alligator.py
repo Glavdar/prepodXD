0e9e4fea37fc6a2770ecd563d6df275ef39090f0
Дмитрий Мартынюк <dima.martynuk2016@gmail.com>
2024-05-19 22:16:56 +0300

    third commit

---
+++ alligator.py
@@ -0,0 +1,8 @@
+# я чисто по приколу буду коммитить что-нибудь
+import webbrowser
+import random
+url='https://store.steampowered.com/app/1895350/I_Wani_Hug_that_Gator/'
+number = random.randint(0, 14)
+print(number)
+if number == 14:
+    webbrowser.open_new_tab(url)
